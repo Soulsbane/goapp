6a2203730d822286b161511d44eb9771efbf5752
Paul Crane <paul@acheronsoft.com>
2021-03-26 03:35:46 -0500

    Add basic type and creation function NewGoApp.

--- goapp.go
+++ goapp.go
@@ -1,5 +1,15 @@
-package main
+package goapp
 
-func main() {
+type GoApp struct {
+	Name string
+	Version string
+}
+
+// NewGoApp returns a new CLI instance with sensible defaults.
+func NewGoApp(app, version string) *GoApp{
+	return &GoApp{
+		Name:         app,
+		Version:      version,
+	}
 
 }
