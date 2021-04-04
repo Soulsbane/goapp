3107a63daa86fa4db5fe7d1d01f8b2a8ee8501d9
Paul Crane <paul@acheronsoft.com>
2021-04-04 15:54:07 -0500

    Rename input to args

--- app/cli.go
+++ app/cli.go
@@ -10,11 +10,11 @@ type GoApp struct {
 }
 
 // NewCmdLineApp returns a new CLI instance with sensible defaults
-func NewCmdLineApp(name string, version string, input interface{}) *GoApp {
+func NewCmdLineApp(name string, version string, args interface{}) *GoApp {
 	var app GoApp
 	app.Name = name
 	app.Version = version
 
-	arg.MustParse(input)
+	arg.MustParse(args)
 	return &app
 }
