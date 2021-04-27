11fb21808286cb9dddba42d7b4578ebcfb92ecd9
Paul Crane <paul@acheronsoft.com>
2021-04-26 23:56:56 -0500

    Initial work on init command

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 	"github.com/Soulsbane/goapp/pkg/cli"
 )
 
-type MyCheckoutCmd struct {
-	Branch string `arg:"positional"`
-	Track  bool   `arg:"-t"`
+type NewAppCommand struct {
+	AppName string `arg:"positional"`
 }
 
 var args struct {
-	Checkout *MyCheckoutCmd `arg:"subcommand:checkout"`
-	Quiet    bool           `arg:"-q"` // this flag is global to all subcommands
+	NewApp *NewAppCommand `arg:"subcommand:init"`
+	Quiet  bool           `arg:"-q"` // this flag is global to all subcommands
 }
 
 func main() {
 	app := cli.NewCmdLineApp("Test App", "1.0", &args)
 	fmt.Println(app.Name)
-	fmt.Println(args.Checkout.Track)
+	fmt.Println(args.NewApp.AppName)
+	fmt.Println(args.Quiet)
 }
