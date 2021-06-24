f9b3fef38d673d2121a485eebae0a151e1fb33e5
Paul Crane <paul@acheronsoft.com>
2021-06-24 03:26:29 -0500

    Add methods PrintInfo, PrintDebug and PrintFatal

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -27,7 +27,22 @@ func (app GoApp) PrintError(msg string) {
 	pterm.Error.Println(msg)
 }
 
+// PrintFatal print a message in light red
+func (app GoApp) PrintFatal(msg string) {
+	pterm.Fatal.Println(msg)
+}
+
 // PrintWarning print a message in yellow
 func (app GoApp) PrintWarning(msg string) {
 	pterm.Warning.Println(msg)
+}
+
+// PrintDebug print a message in gray
+func (app GoApp) PrintDebug(msg string) {
+	pterm.Debug.Println(msg)
+}
+
+// PrintInfo print a message in cyan
+func (app GoApp) PrintInfo(msg string) {
+	pterm.Info.Println(msg)
 }
