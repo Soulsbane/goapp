ca656ec9e8202d574e00d80fa65b5f658c24792f
Paul Crane <paul@acheronsoft.com>
2022-02-27 03:22:32 -0600

    Complete reworking of Print methods

--- go.mod
+++ go.mod
@@ -4,9 +4,7 @@ go 1.16
 
 require (
 	github.com/alexflint/go-arg v1.4.1
-	github.com/gookit/color v1.4.2 // indirect
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db
-	github.com/pterm/pterm v0.12.13
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
-	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- pkg/cli/io.go
+++ pkg/cli/io.go
@@ -1,39 +1,61 @@
 package cli
 
 import (
+	"runtime"
+	"strconv"
+
 	"github.com/mitchellh/colorstring"
-	"github.com/pterm/pterm"
+)
+
+const (
+	callerInfoColor = "[green]"
 )
 
 // Println Print a message using mitchellh/colorstring tags
-func (app GoApp) Println(msg string) {
-	colorstring.Println(msg)
+func (app GoApp) Println(msg ...interface{}) {
+	var args string
+
+	for _, arg := range msg {
+		args += arg.(string)
+	}
+
+	colorized := colorstring.Color(args)
+	colorstring.Println(colorized)
+}
+
+func (app GoApp) PrintCallerInfo() {
+	_, fileName, line, _ := runtime.Caller(2)
+	app.Println(callerInfoColor, "└(", fileName, ":", strconv.Itoa(line), ")")
 }
 
 // PrintError print a message in red
 func (app GoApp) PrintError(msg string) {
-	pterm.Error.Println(msg)
+	app.Println("[_red_][black]  ERROR  [_default_][light_red] " + msg)
+	app.PrintCallerInfo()
 }
 
 // PrintFatal print a message in light red
 func (app GoApp) PrintFatal(msg string) {
-	pterm.Fatal.Println(msg)
+	app.Println("[_red_][black]  FATAL  [_default_][light_red] " + msg)
+	app.PrintCallerInfo()
 }
 
 // PrintWarning print a message in yellow
 func (app GoApp) PrintWarning(msg string) {
-	pterm.Warning.Println(msg)
+	app.Println("[_yellow_][black]  WARNING  [_default_][yellow] " + msg)
+	app.PrintCallerInfo()
 }
 
 // PrintDebug print a message in gray
 func (app GoApp) PrintDebug(msg string) {
 	if app.IsDebugModeEnabled() {
-		pterm.EnableDebugMessages()
-		pterm.Debug.Println(msg)
+		app.Println("[_light_red_][black]  DEBUG  [_default_][_light_red_] " + msg)
+		app.PrintCallerInfo()
 	}
 }
 
 // PrintInfo print a message in cyan
 func (app GoApp) PrintInfo(msg string) {
-	pterm.Info.Println(msg)
+	app.Println("[_cyan_][black]  INFO  [_default_][light_cyan] " + msg)
+	app.PrintCallerInfo()
 }
