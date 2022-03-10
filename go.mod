57a8294efd1c5293abbfa2d1cee9753ba9edc40c
Paul Crane <paul@acheronsoft.com>
2022-03-10 00:52:31 -0600

    Add better error handling in SaveConfigFile and OpenConfigFile

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/alexflint/go-arg v1.4.1
+	github.com/hashicorp/go-multierror v1.1.1
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.6
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- pkg/config/file.go
+++ pkg/config/file.go
@@ -3,27 +3,50 @@ package config
 import (
 	"io/ioutil"
 
+	"github.com/hashicorp/go-multierror"
 	toml "github.com/pelletier/go-toml/v2"
 )
 
-func (config Config) OpenConfigFile(values interface{}) {
-	fileName, _ := config.GetUserConfigFilePath()
-	data, _ := ioutil.ReadFile(fileName)
+func (config Config) OpenConfigFile(values interface{}) error {
+	var result *multierror.Error
 
-	err := toml.Unmarshal(data, values)
+	fileName, err := config.GetUserConfigFilePath()
 
 	if err != nil {
-		panic(err)
+		result = multierror.Append(result, err)
 	}
+
+	data, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		result = multierror.Append(result, err)
+	}
+
+	err = toml.Unmarshal(data, values)
+
+	if err != nil {
+		result = multierror.Append(result, err)
+	}
+
+	return result.ErrorOrNil()
 }
 
-func (config *Config) SaveConfigFile(values interface{}) {
-	fileName, _ := config.GetUserConfigFilePath()
+func (config *Config) SaveConfigFile(values interface{}) error {
+	var result *multierror.Error
+
+	fileName, err := config.GetUserConfigFilePath()
+
+	if err != nil {
+		result = multierror.Append(result, err)
+	}
+
 	data, err := toml.Marshal(&values)
 
 	if err != nil {
-		panic(err)
+		result = multierror.Append(result, err)
 	}
 
 	ioutil.WriteFile(fileName, data, 0666)
+
+	return result.ErrorOrNil()
 }
