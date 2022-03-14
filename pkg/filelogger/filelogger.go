175d740b5473e38cb8eba863f1fdece365e3c889
Paul Crane <paul@acheronsoft.com>
2022-03-13 22:12:05 -0500

    Add additional error return value to filelogger.New

--- pkg/filelogger/filelogger.go
+++ pkg/filelogger/filelogger.go
@@ -11,18 +11,14 @@ type FileLogger struct {
 	logger *log.Logger
 }
 
-func New(fileName string, directory string, flag int) FileLogger {
+func New(fileName string, directory string, flag int) (FileLogger, error) {
+	var fileLogger FileLogger
+
 	os.MkdirAll(directory, os.ModePerm)
 	file, err := os.OpenFile(path.Join(directory, fileName), flag|os.O_CREATE|os.O_WRONLY, 0644)
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	var fileLogger FileLogger
 	fileLogger.logger = log.New(file, "", log.LstdFlags)
 
-	return fileLogger
+	return fileLogger, err
 }
 
 func (fileLogger FileLogger) Println(msg ...interface{}) {
