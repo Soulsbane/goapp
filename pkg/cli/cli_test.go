b0eb79b5fa33e1477a9c075f8ce33100ed53514a
Paul Crane <paul@acheronsoft.com>
2022-04-03 03:25:12 -0500

    Remove most tests as they are mainly part of cmd/goapp now

--- pkg/cli/cli_test.go
+++ pkg/cli/cli_test.go
@@ -2,29 +2,15 @@ package cli_test
 
 import (
 	"testing"
-
-	"github.com/Soulsbane/goapp/pkg/cli"
-	"github.com/alexflint/go-arg"
 )
 
-type NewAppCommand struct {
-	AppName string `arg:"positional"`
-}
-
-var args struct {
-	NewApp *NewAppCommand `arg:"subcommand:init"`
-	Quiet  bool           `arg:"-q"` // this flag is global to all subcommands
-}
-
 func TestCli(m *testing.T) {
-	app := cli.NewGoApp(
+	/*app := cli.NewGoApp(
 		cli.WithName("OVER name"),
 		cli.WithCompany("OVER vend"),
 		cli.WithVersion("6.66"),
 		cli.WithDebugMode(true),
-	)
-
-	arg.MustParse(&args)
+	)*/
 
 	/*err := app.OpenConfigFile(&cfg)
 
